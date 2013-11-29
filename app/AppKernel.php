52ac92ca7a0fe3f63076804be71f86144044a6f1
bnthms88 <ben@hocorunning.com>
2013-11-29 18:49:11 +0000

    Got files working on Bighorn

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Archer\MeetEntryBundle\ArcherMeetEntryBundle(),
+            new Pinano\Bootstrap3Bundle\PinanoBootstrap3Bundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
+
 if (!isset($_SERVER['HTTP_HOST'])) {
     exit('This script cannot be run from the CLI. Run it from a browser.');
 }
 
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
-    '192.168.126.1',
     '127.0.0.1',
     '::1',
 ))) {
