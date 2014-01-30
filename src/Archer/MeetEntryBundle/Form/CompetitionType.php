3a6a48f808b23eb2bc21e19d8aa2199f6cb950c0
bnthms88 <ben@hocorunning.com>
2014-01-30 04:58:26 +0000

    using competition to test the design and ajax/js mgmt

--- src/Archer/MeetEntryBundle/Form/CompetitionType.php
+++ src/Archer/MeetEntryBundle/Form/CompetitionType.php
@@ -5,6 +5,7 @@ namespace Archer\MeetEntryBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Archer\MeetEntryBundle\Form\DataTransformer\StringToArrayTransformer;
 
 class CompetitionType extends AbstractType
 {
@@ -14,13 +15,26 @@ class CompetitionType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $transformer = new StringToArrayTransformer();
         $builder
             ->add('name')
             ->add('location')
-            ->add('start_date')
-            ->add('end_date')
-            ->add('registration_start_date')
-            ->add('registration_end_date')
+            ->add('start_date', 'datetime', array(
+              'date_widget'=>'single_text',
+              'time_widget'=>'single_text'
+            ))
+            ->add('end_date', 'datetime', array(
+              'date_widget'=>'single_text',
+              'time_widget'=>'single_text'
+            ))
+            ->add('registration_start_date', 'datetime', array(
+              'date_widget'=>'single_text',
+              'time_widget'=>'single_text'
+            ))
+            ->add('registration_end_date', 'datetime', array(
+              'date_widget'=>'single_text',
+              'time_widget'=>'single_text'
+            ))
             ->add('registration_code')
             ->add('notes')
             ->add('events_per_athlete')
@@ -29,6 +43,7 @@ class CompetitionType extends AbstractType
             ->add('athletes_per_event')
             ->add('relays_per_event')
             ->add('competition_type')
+            
         ;
     }
     
